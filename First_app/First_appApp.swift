ffdfc70ccbe0aec0e77da7f42697b4eca01e7182
Ekaterina Tarasova <ekaterinatarasova@MacBook-Pro-Ekaterina.local>
2021-01-23 13:45:40 +0300

    check

---
+++ First_app/First_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_appApp.swift
+//  First_app
+//
+//  Created by Ekaterina Tarasova on 23.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct First_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
