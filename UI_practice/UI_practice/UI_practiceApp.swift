80978db22ec3bbc58598b28025fcf6e35ec22617
katringht <katrin_tko@mail.ru>
2021-01-31 21:36:34 +0300

    design practice

---
+++ UI_practice/UI_practice/UI_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_practiceApp.swift
+//  UI_practice
+//
+//  Created by Ekaterina Tarasova on 31.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct UI_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
